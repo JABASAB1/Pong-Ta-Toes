51bd51496e8f6c5e70e317cbe99109d94eebb694
howardzacharyj <howardzacharyj@gmail.com>
2015-11-19 16:28:35 -0500

    game mechanics

--- Bonus/angularSeedApp/app/game/game.js
+++ Bonus/angularSeedApp/app/game/game.js
@@ -30,35 +30,86 @@ angular.module('myApp.game', ['ngRoute'])
         return { w: this.w, h: this.h };
     }
 
-    Potato.prototype.move = function () {
-        this.x += this.vX;
-        this.y += this.vY;
-        if (this.x + this.w*.3 < 0) {
-            $scope.player2.awardPoint();
-            $scope.potato = new Potato(potatoX, potatoY, -1, 0, potatoWidth, potatoHeight);
-        } else if (this.x + this.w*.7 > $scope.width) {
-            $scope.player1.awardPoint();
-            $scope.potato = new Potato(potatoX, potatoY, 1, 0, potatoWidth, potatoHeight);
+    Potato.prototype.bounce = function (paddle) {
+        if (paddle) {
+            if (this.vX > 0) {
+                this.vX + .2;
+            } else {
+                this.vX - .2;
+            }
+            this.vX = (this.vX) * -1;
+        }
+        
+
+        if (paddle) {
+            var startingV = this.vY;
+            if (startingV == 0) {
+                if (Math.random() > .5) {
+                    startingV = Math.random()/2;
+                } else {
+                    startingV = -Math.random()/2;
+                }
+            }
+
+            this.vY = startingV + paddle.getVelocity()/2;
+
+        } else {
+            this.vY = this.vY * -1;
+
         }
     }
 
-    Potato.prototype.accelarate = function () {
+    Potato.prototype.move = function () {
 
+        if (this.vX > 0) {
+            if($scope.player2.getPaddle().collide(this)){
+                this.bounce($scope.player2.getPaddle());
+            }
+        } else {
+            if($scope.player1.getPaddle().collide(this)){
+                this.bounce($scope.player1.getPaddle());
+            }
+        }
+
+        if (this.vY < 0) {
+            if (this.y < 0) {
+                this.bounce();
+            }
+        } else {
+            if (this.y + this.h > $scope.height) {
+                this.bounce();
+            }
+        }
+
+        this.x += this.vX;
+        this.y += this.vY;
+
+        if (this.x < 0) {
+            $scope.player2.awardPoint();
+            $scope.potato = new Potato(potatoX, potatoY, -.5, 0, potatoWidth, potatoHeight);
+        } else if (this.x + this.w > $scope.width) {
+            $scope.player1.awardPoint();
+            $scope.potato = new Potato(potatoX, potatoY, .5, 0, potatoWidth, potatoHeight);
+        }
     }
 
-    var potatoWidth = $scope.width / 10;
+    Potato.prototype.getVelocity = function () {
+        return { x: this.vX, y: this.vY };
+    }
+
     var potatoHeight = $scope.height / 10;
+    var potatoWidth = potatoHeight;
 
     var potatoX = ($scope.width / 2) - (potatoWidth / 2);
     var potatoY = ($scope.height / 2) - (potatoHeight / 2);
 
-    $scope.potato = new Potato(potatoX, potatoY, 1, 0, potatoWidth, potatoHeight);
+    $scope.potato = new Potato(potatoX, potatoY, .5, 0, potatoWidth, potatoHeight);
 
     $interval(function () {
         $scope.potato.move();
         $scope.player1.getPaddle().move();
         $scope.player2.getPaddle().move();
-    }, 10);
+    }, 5);
 
     function Player(paddle, score) {
         this.paddle = paddle;
@@ -91,9 +142,9 @@ angular.module('myApp.game', ['ngRoute'])
 
     Paddle.prototype.accelerate = function (up) {
         if (up) {
-            this.v = -2;
+            this.v = -1;
         } else {
-            this.v = 2;
+            this.v = 1;
         }
     }
 
@@ -105,6 +156,10 @@ angular.module('myApp.game', ['ngRoute'])
         }
     }
 
+    Paddle.prototype.getVelocity = function () {
+        return this.v;
+    }
+
     var paddleWidth = $scope.width / 50;
     var paddleHeight = $scope.height / 5;
     var paddle1X = paddleWidth;
@@ -114,8 +169,24 @@ angular.module('myApp.game', ['ngRoute'])
     Paddle.prototype.move = function () {
         if (this.y + this.v >= 0 && this.y + this.v + this.h <= $scope.height) {
             this.y += this.v;
-        } else {
         }
+    }
+
+    Paddle.prototype.collide = function (potato) {
+        if (potato.getVelocity().x > 0) {
+            if (potato.getCoordinates().x + potato.getSize().w > this.x) {
+                if (potato.getCoordinates().y + potato.getSize().h > this.y && potato.getCoordinates().y < this.y + this.h) {
+                    return true;
+                }
+            }
+        } else {
+            if (potato.getCoordinates().x < this.x + this.w) {
+                if (potato.getCoordinates().y + potato.getSize().h > this.y && potato.getCoordinates().y < this.y + this.h) {
+                    return true;
+                }
+            }
+        }
+        
     }
 
     var paddle1 = new Paddle(paddle1X, paddleY, 0, paddleWidth, paddleHeight);
