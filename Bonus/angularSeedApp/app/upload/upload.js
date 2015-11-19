586d9c8e24dd8404f08c7d28dee76b1032e38726
JABASAB1 <jtbarron77@gmail.com>
2015-11-19 15:59:29 -0500

    everything works!

--- Bonus/angularSeedApp/app/upload/upload.js
+++ Bonus/angularSeedApp/app/upload/upload.js
@@ -8,4 +8,11 @@ angular.module('myApp.upload', ['ngRoute'])
         controller: 'uploadController'
     });
 }])
-.controller('uploadController', ["$scope", "$interval", "$window", function ($scope, $interval, $window) {}]);
+.controller('uploadController', ["$scope", "$interval", "$window", function ($scope, $interval, $window) {
+    $scope.samPicture = "Unset";
+    $scope.gollumPicture = "Unset";
+
+    $scope.debug = function () {
+        alert("Sam Picture: " + $scope.samPicture + " Gollum Picture: " + $scope.gollumPicture);
+    }
+}]);
