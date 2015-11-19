e002fd0ad2755dc420b3f5df825136c4dcf95afc
Pippin99 <git@ruehl.us>
2015-11-19 14:57:37 -0500

    add stuff

--- Bonus/angularSeedApp/app/home/home.js
+++ Bonus/angularSeedApp/app/home/home.js
@@ -8,3 +8,9 @@ angular.module('myApp.home', ['ngRoute'])
         controller: 'gameController'
     });
 }]);
+
+$scope.player1score = angular.module('gameModule').player1score;
+$scope.player2score = 0;
+
+$scope.player1image = "sam.png";
+$scope.player2image = "gollum.png";
