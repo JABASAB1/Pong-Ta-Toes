11e3eab3ce38930d3ceac5c76cb62015dde688bb
howardzacharyj <howardzacharyj@gmail.com>
2015-11-19 11:04:42 -0500

    js for game

--- Bonus/angularSeedApp/app/game/view1.js
+++ Bonus/angularSeedApp/app/game/view1.js
@@ -1,14 +1,60 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+    });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View2Ctrl', ["$scope", "$interval", "$window", function ($scope, $interval, $window) {
+    $scope.height = $window.innerHeight * .8;
+    $scope.width = $window.innerWidth * .8;
+
+    function Potato(x, y, vX, vY, w, h) {
+        this.x = x;
+        this.y = y;
+        this.vX = vX;
+        this.vY = vY;
+        this.w = w;
+        this.h = h;
+    };
+
+    Potato.prototype.getCoordinates = function () {
+        return { x: this.x, y: this.y };
+    }
+
+    Potato.prototype.getSize = function () {
+        return { w: this.w, h: this.h };
+    }
+
+    Potato.prototype.move = function () {
+        this.x += this.vX;
+        this.y += this.vY;
+    }
+
+    Potato.prototype.accelarate = function () {
+
+    }
+
+    var w = $scope.width / 10;
+    var h = $scope.height / 10;
+
+    var x = ($scope.width / 2) - (w / 2);
+    var y = ($scope.height / 2) - (h / 2);
+
+    $scope.potato = new Potato(x, y, 1, 0, w, h);
+
+    $interval(function () {
+        $scope.potato.move();
+    }, 10);
+
+    function Player(paddle) {
+        this.paddle = paddle;
+    }
+
+    Player.prototype.getPaddle()
 
 }]);
